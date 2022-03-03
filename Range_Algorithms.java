5bff6507ea04be1f606ba2128a8e849c16dd8ee3
danfilienko <daniilf@uw.edu>
2022-03-02 17:25:38 -0800

    Added graphs and data

--- Range_Algorithms.java
+++ Range_Algorithms.java
@@ -1,22 +1,55 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Random;
+
 public class Range_Algorithms {
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws IOException
+    {
+        //Declare the array of values simulating the amount values passed in a function as was presented in a presentation
         int[] testValuesInefficient = {1000, 2000, 4000, 8000, 16000, 32000, 64000};
         int[] testValuesEfficient = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000, 256000, 512000, 1000000, 2000000, 4000000, 8000000, 16700000, 33000000, 65000000, 130000000, 260000000};
 
-        System.out.println("Inefficient Range Algorithm testing");
+        Random rand = new Random();
 
-        inefficientRange(testValuesInefficient);
+        writeString("EFFICIENT RANGE VALUES");
+        for (int num: testValuesEfficient){
+            //Limiting the value to 100 to make the generation of random numbers more efficient, as numbers utilized in an exmaple
+            //were not specified
+            int[] workingArr = new Random().ints(num,1,1000).toArray();
 
-        System.out.println("Less inefficient Range Algorithm testing");
+            final long startTime3 = System.currentTimeMillis();
+            efficientRange(workingArr);
+            final long endTime3 = System.currentTimeMillis();
+            writeString(""+(endTime3 - startTime3));
+        }
+        //Divider between efficient and inefficient values
+        writeString("_____________________________");
 
-        updatedInefficientRange(testValuesInefficient);
+        writeString("MILDLYINEFFICIENT RANGE VALUES");
+        for (int num: testValuesInefficient){
+            int[] workingArr = new Random().ints(num).toArray();
+            //Utilize System.CurrentTimeMillis to access appropriate time changes from the moment a range ran
+            //to the moment an output was produced
+            final long startTime2 = System.currentTimeMillis();
+            updatedInefficientRange(workingArr);
+            final long endTime2 = System.currentTimeMillis();
+            writeString(""+(endTime2 - startTime2));
+        }
+        writeString("_____________________________");
 
-        System.out.println("Efficient Range Algorithm testing");
-
-        efficientRange(testValuesEfficient);
-
+        writeString("INEFFICIENT RANGE VALUES");
+        for (int num: testValuesInefficient) {
+            int[] workingArr = new Random().ints(num).toArray();
+            //Utilize System.CurrentTimeMillis to access appropriate time changes from the moment a range ran
+            //to the moment an output was produced
+            final long startTime = System.currentTimeMillis();
+            inefficientRange(workingArr);
+            final long endTime = System.currentTimeMillis();
+            writeString(""+(endTime - startTime));
+        }
     }
 
     static int inefficientRange(int[] numbers) {
@@ -56,6 +89,15 @@ public class Range_Algorithms {
             }
         }
         return max - min;
+    }
+    public static void writeString(String value)
+            throws IOException
+    {
+        // Writing into the file
+        String filename= "Logs.txt";
+        FileWriter fw = new FileWriter(filename,true); //the true will append the new data
+        fw.write(value + "\n");//appends the string to the file
+        fw.close();
     }
 
 }
